b193cde7f5e835ae90381dbb726256d21b40f948
diegopau <diegoocampo8@gmail.com>
2014-04-19 15:00:22 +0200

    ready to work in the public beta prototype

---
+++ html/Latest simulated prototype/resources/scroll-1397406690468.js
@@ -0,0 +1,36 @@
+(function(window, undefined) {
+
+    /*********************** START STATIC ACCESS METHODS ************************/
+
+    jQuery.extend(jimMobile, {
+        "loadScrollBars": function() {
+            jQuery(".s-d12245cc-1680-458d-89dd-4f0d7fb22724 .ui-page").overscroll({ showThumbs:true, direction:'multi' });
+            jQuery(".s-d12245cc-1680-458d-89dd-4f0d7fb22724 #s-Menu").overscroll({ showThumbs:false, direction:'vertical' });
+            jQuery(".s-d12245cc-1680-458d-89dd-4f0d7fb22724 #s-Notificaciones_1").overscroll({ showThumbs:false, direction:'vertical' });
+            jQuery(".s-d12245cc-1680-458d-89dd-4f0d7fb22724 #s-TimeLine").overscroll({ showThumbs:false, direction:'vertical' });
+            jQuery(".s-d12245cc-1680-458d-89dd-4f0d7fb22724 #s-ChatPublico").overscroll({ showThumbs:false, direction:'vertical' });
+            jQuery(".s-d12245cc-1680-458d-89dd-4f0d7fb22724 #s-ConversacionPublico").overscroll({ showThumbs:false, direction:'vertical' });
+            jQuery(".s-d12245cc-1680-458d-89dd-4f0d7fb22724 #s-ConversacionGrupo").overscroll({ showThumbs:false, direction:'vertical' });
+            jQuery(".s-d12245cc-1680-458d-89dd-4f0d7fb22724 #s-ConversacionUsuario").overscroll({ showThumbs:false, direction:'vertical' });
+            jQuery(".s-d12245cc-1680-458d-89dd-4f0d7fb22724 #s-ConversacionMate").overscroll({ showThumbs:false, direction:'vertical' });
+            jQuery(".s-d12245cc-1680-458d-89dd-4f0d7fb22724 #s-MiPerfil").overscroll({ showThumbs:false, direction:'vertical' });
+            jQuery(".s-d12245cc-1680-458d-89dd-4f0d7fb22724 #s-ContenidoPrivado").overscroll({ showThumbs:false, direction:'vertical' });
+            jQuery(".s-d12245cc-1680-458d-89dd-4f0d7fb22724 #s-ContenidoPublico").overscroll({ showThumbs:false, direction:'vertical' });
+            jQuery(".s-d12245cc-1680-458d-89dd-4f0d7fb22724 #s-UserPublicProfile").overscroll({ showThumbs:false, direction:'vertical' });
+            jQuery(".s-d12245cc-1680-458d-89dd-4f0d7fb22724 #s-TimeLine_1").overscroll({ showThumbs:false, direction:'vertical' });
+            jQuery(".s-d12245cc-1680-458d-89dd-4f0d7fb22724 #s-ChatPublico_1").overscroll({ showThumbs:false, direction:'vertical' });
+            jQuery(".s-d12245cc-1680-458d-89dd-4f0d7fb22724 #s-ConversacionPublico_1").overscroll({ showThumbs:false, direction:'vertical' });
+            jQuery(".s-d12245cc-1680-458d-89dd-4f0d7fb22724 #s-ConversacionGrupo_1").overscroll({ showThumbs:false, direction:'vertical' });
+            jQuery(".s-d12245cc-1680-458d-89dd-4f0d7fb22724 #s-ConversacionUsuario_1").overscroll({ showThumbs:false, direction:'vertical' });
+            jQuery(".s-d12245cc-1680-458d-89dd-4f0d7fb22724 #s-ConversacionMate_1").overscroll({ showThumbs:false, direction:'vertical' });
+            jQuery(".s-d12245cc-1680-458d-89dd-4f0d7fb22724 #s-Chats").overscroll({ showThumbs:false, direction:'vertical' });
+            jQuery(".s-d12245cc-1680-458d-89dd-4f0d7fb22724 #s-Hives").overscroll({ showThumbs:false, direction:'vertical' });
+            jQuery(".s-d12245cc-1680-458d-89dd-4f0d7fb22724 #s-Mates").overscroll({ showThumbs:false, direction:'vertical' });
+            jQuery(".s-d12245cc-1680-458d-89dd-4f0d7fb22724 #s-ContenidoTabs").overscroll({ showThumbs:false, direction:'vertical' });
+            jQuery(".s-7968ff12-fb1c-475f-9d22-684443388b92 .ui-page").overscroll({ showThumbs:true, direction:'multi' });
+         }
+    });
+
+    /*********************** END STATIC ACCESS METHODS ************************/
+
+}) (window);
